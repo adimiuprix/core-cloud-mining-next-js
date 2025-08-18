abac1e080e7635d7a6816505ec4a4910e5952fa2
adimiuprix <miuprix@gmail.com>
2025-08-18 12:51:51 +0700

    Schema prisma

---
+++ prisma/migrations/20250818054146_init/migration.sql
@@ -0,0 +1,45 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NULL,
+    `balance` DECIMAL(20, 8) NOT NULL DEFAULT 0.00000000,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Plan` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `plan_name` VARCHAR(191) NOT NULL,
+    `earn_per_day` DECIMAL(20, 8) NOT NULL,
+    `earning_rate` DECIMAL(20, 8) NOT NULL,
+    `price` DECIMAL(20, 8) NOT NULL,
+    `duration` INTEGER NOT NULL,
+    `profit` DECIMAL(20, 8) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `user_mining_record` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `plan_id` INTEGER NOT NULL,
+    `status` ENUM('active', 'inactive') NOT NULL DEFAULT 'active',
+    `last_sum` BIGINT NOT NULL,
+    `expire_date` DATETIME(3) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `user_mining_record` ADD CONSTRAINT `user_mining_record_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `user_mining_record` ADD CONSTRAINT `user_mining_record_plan_id_fkey` FOREIGN KEY (`plan_id`) REFERENCES `Plan`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/seed.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.plan.create({
+    data: {
+      plan_name: "Plan 1",
+      earn_per_day: 0.50000000,
+      earning_rate: 1.50000000,
+      price: 100.00000000,
+      duration: 1,
+      profit: 15.00000000,
+    }
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
